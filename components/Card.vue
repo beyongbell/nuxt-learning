5eb360096c1229a937180f612892a47334de195f
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-08 23:00:29 +0700

    Show Post Id

--- components/Card.vue
+++ components/Card.vue
@@ -3,7 +3,9 @@
         <div class="card bg-light mb-3" style="max-width: 18rem;">
             <div class="card-header"> Post : {{ post.id }}</div>
             <div class="card-body">
-                <h5 class="card-title">{{ post.title }}</h5>
+                <h5 class="card-title">
+                    <nuxt-link :to="{ name: 'posts-id', params: { id: post.id }}"> {{ post.title }} </nuxt-link>
+                </h5>
             </div>
         </div>
     </div>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        {{ $route.params.id }}
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
