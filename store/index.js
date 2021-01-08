ce88c08c22acc1e17209a8db3036345d3a281ea5
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-08 23:51:46 +0700

    Create vuex store

---
+++ store/index.js
@@ -0,0 +1,25 @@
+// states
+export const state = () => ({
+    posts: {}
+});
+
+// getters
+export const getters = {
+    posts(state) {
+        return state.posts
+    }
+}
+
+// mutations
+export const mutations = {
+    SET_POSTS(state, posts) {
+        state.posts = posts
+    }
+}
+
+// actions
+export const actions = {
+    setPosts({commit}, posts) {
+        commit("SET_POSTS", posts)
+    }
+}
