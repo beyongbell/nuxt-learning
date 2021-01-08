9f3e38edcfa3ad2426fa633509d49711f6842ee2
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-08 20:23:05 +0700

    Route Parameter

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,11 +1,22 @@
 <template lang="">
     <div>
         <h1> Hello Users </h1>
+        <input type="text" v-model="id">
+        <button @click="loadUser"> Load User </button>
     </div>
 </template>
 <script>
 export default {
-    
+    data() {
+        return {
+            id: ''
+        }
+    },
+    methods: {
+        loadUser() {
+            this.$router.push(`/users/${this.id}`)
+        }
+    }
 }
 </script>
 <style lang="">
