66521e5d6c062abe85e3996d9940c9918d980497
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-08 20:56:39 +0700

    Custom Error Page

--- components/Nav.vue
+++ components/Nav.vue
@@ -11,7 +11,7 @@
                 <a class="nav-link" href="/">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="users">Users</a>
+                <a class="nav-link" href="/users">Users</a>
             </li>
             </ul>
         </div>
---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div class="container">
+        <h2> 404 PAGE NOT FOUND! </h2>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
