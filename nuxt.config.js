2d9200f855fadc903266092f5f2704e8ca4569a1
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2021-01-09 15:38:38 +0700

    Applying transitions

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,6 +33,11 @@ export default {
     '@/assets/styles/main.css'
   ],
 
+  transition: {
+    name: 'fade',
+    mode: 'out-in'
+  },
+
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     '@/plugins/scrollto.js',
